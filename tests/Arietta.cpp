5b23f421244587e2113954b992b98f986da30b06
Yiheng Wu <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-05-29 18:57:13 +0800

    feat(Tests): Add testing framework based on `boost-ext/ut`. (#2)
    1. Define and test aliases for fundamental types in `Arietta.h`.

---
+++ tests/Arietta.cpp
@@ -0,0 +1,42 @@
+#include "Arietta/Arietta.h"
+
+#include <boost/ut.hpp>
+
+using namespace arietta;
+using namespace boost::ut;
+
+namespace {
+
+suite<"Arietta"> _ = [] {
+  "Aliases"_test = [] {
+    static_assert(sizeof(i8) == 1);
+    static_assert(sizeof(u8) == 1);
+    static_assert(sizeof(i16) == 2);
+    static_assert(sizeof(u16) == 2);
+    static_assert(sizeof(i32) == 4);
+    static_assert(sizeof(u32) == 4);
+    static_assert(sizeof(i64) == 8);
+    static_assert(sizeof(u64) == 8);
+    static_assert(sizeof(isize) == sizeof(void *));
+    static_assert(sizeof(usize) == sizeof(void *));
+
+    static_assert(std::is_signed_v<i8>);
+    static_assert(!std::is_signed_v<u8>);
+    static_assert(std::is_signed_v<i16>);
+    static_assert(!std::is_signed_v<u16>);
+    static_assert(std::is_signed_v<i32>);
+    static_assert(!std::is_signed_v<u32>);
+    static_assert(std::is_signed_v<i64>);
+    static_assert(!std::is_signed_v<u64>);
+    static_assert(std::is_signed_v<isize>);
+    static_assert(!std::is_signed_v<usize>);
+
+    static_assert(sizeof(f32) == 4);
+    static_assert(sizeof(f64) == 8);
+
+    static_assert(std::is_signed_v<f32>);
+    static_assert(std::is_signed_v<f64>);
+  };
+};
+
+} // namespace
