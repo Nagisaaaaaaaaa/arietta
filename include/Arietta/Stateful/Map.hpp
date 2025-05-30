0f1f75ef018ddea4cdfb2a78ea46f2ad9c520626
Yiheng Wu <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-05-30 15:40:30 +0800

    feat(Next): Simplify the implementation of `Next()`. (#6)

--- include/Arietta/Stateful/Map.hpp
+++ include/Arietta/Stateful/Map.hpp
@@ -23,13 +23,25 @@ struct Setter {
 
 struct Default {};
 
+template <auto v>
+struct C {};
+
 } // namespace map::detail
 
+// TODO: Currently, template parameters in C++ classes can only be either `typename` or `auto`.
+//       In other words, they do not truly support overloading in the same way functions do.
+//       As a result, the template parameters of `class Map` and `Find`
+//       currently support only `typename`, not `auto`.
 template <typename T = map::detail::Default>
 struct Map {
   template <typename Key, typename Value>
   static consteval void Insert() {
     [[maybe_unused]] map::detail::Setter<T, Key, Value> s;
+  }
+
+  template <auto key, typename Value>
+  static consteval void Insert() {
+    Insert<map::detail::C<key>, Value>();
   }
 
   template <typename Key>
