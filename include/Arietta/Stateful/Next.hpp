2d39ef7e7463e2d33362cae37765d2274b3cf8dc
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-05-30 13:38:53 +0800

    Support multiple counters in a single TU.

--- include/Arietta/Stateful/Next.hpp
+++ include/Arietta/Stateful/Next.hpp
@@ -6,31 +6,41 @@ namespace arietta::stateful {
 
 namespace next::detail {
 
-template <usize n>
+template <typename T, usize i>
 struct Reader {
-  friend auto flag(Reader<n>);
+  friend auto flag(Reader<T, i>);
 };
 
-template <usize n>
+template <typename T, usize i>
 struct Setter {
-  friend auto flag(Reader<n>) {}
+  friend auto flag(Reader<T, i>) {}
 
-  static constexpr usize value = n;
+  static constexpr usize value = i;
 };
 
-template <auto tag, usize i = 0>
+template <typename T, auto tag, usize i = 0>
 [[nodiscard]] consteval auto NextImpl() {
-  if constexpr (requires(Reader<i> r) { flag(r); }) {
-    return NextImpl<tag, i + 1>();
+  if constexpr (requires(Reader<T, i> r) { flag(r); }) {
+    return NextImpl<T, tag, i + 1>();
   } else {
-    Setter<i> s;
+    Setter<T, i> s;
     return s.value;
   }
 }
 
+struct Default {};
+
+template <auto v>
+struct C {};
+
 } // namespace next::detail
 
-template <auto tag = []() {}, auto res = next::detail::NextImpl<tag>()>
+template <typename T = next::detail::Default, auto tag = []() {}, auto res = next::detail::NextImpl<T, tag>()>
+consteval auto Next() {
+  return res;
+};
+
+template <auto v, auto tag = []() {}, auto res = next::detail::NextImpl<next::detail::C<v>, tag>()>
 consteval auto Next() {
   return res;
 };
--- tests/Stateful/Next.cpp
+++ tests/Stateful/Next.cpp
@@ -10,10 +10,26 @@ namespace {
 suite<"Next"> _ = [] {
   "Next"_test = [] {
     static_assert(stateful::Next() == 0);
+    static_assert(stateful::Next<int>() == 0);
+    static_assert(stateful::Next<float>() == 0);
+    static_assert(stateful::Next<0>() == 0);
+    static_assert(stateful::Next<3.14F>() == 0);
+
     static_assert(stateful::Next() == 1);
+    static_assert(stateful::Next<int>() == 1);
+    static_assert(stateful::Next<int>() == 2);
+    static_assert(stateful::Next<float>() == 1);
+    static_assert(stateful::Next<float>() == 2);
+    static_assert(stateful::Next<float>() == 3);
+    static_assert(stateful::Next<0>() == 1);
+    static_assert(stateful::Next<0>() == 2);
+    static_assert(stateful::Next<3.14F>() == 1);
+
     static_assert(stateful::Next() == 2);
-    static_assert(stateful::Next() == 3);
-    static_assert(stateful::Next() == 4);
+    static_assert(stateful::Next<int>() == 3);
+    static_assert(stateful::Next<float>() == 4);
+    static_assert(stateful::Next<0>() == 3);
+    static_assert(stateful::Next<3.14F>() == 2);
   };
 };
 
