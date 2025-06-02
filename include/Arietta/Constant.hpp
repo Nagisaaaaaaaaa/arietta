6bfde6d95d3d0f88ea16d3c6ec6eac2e439cf5ca
Wyh <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-06-02 17:15:02 +0800

    Define operators for `(op C)`, `(C op)`, and `(C op C)`.

--- include/Arietta/Constant.hpp
+++ include/Arietta/Constant.hpp
@@ -20,7 +20,63 @@ struct C {
 
   static constexpr auto value = v;
 
-  [[nodiscard]] ART_SPECIFIER constexpr operator value_type() const noexcept { return value; }
+  [[nodiscard]] ART_SPECIFIER consteval operator value_type() const noexcept { return value; }
 };
+
+//
+//
+//
+// Operators for `(op C)`, `(C op)`, and `(C op C)`.
+
+#if defined(ART_LEFT_UNARY_OP) || defined(ART_RIGHT_UNARY_OP) || defined(ART_BINARY_OP)
+static_assert(false, "Some macros conflict with Arietta");
+#endif
+
+#define ART_LEFT_UNARY_OP(OP)                                                                                          \
+  template <auto t>                                                                                                    \
+  [[nodiscard]] ART_SPECIFIER consteval C<(OP t)> operator OP(C<t>) {                                                  \
+    return {};                                                                                                         \
+  }
+#define ART_RIGHT_UNARY_OP(OP)                                                                                         \
+  template <auto t>                                                                                                    \
+  [[nodiscard]] ART_SPECIFIER consteval C<(t OP)> operator OP(C<t>) {                                                  \
+    return {};                                                                                                         \
+  }
+#define ART_BINARY_OP(OP)                                                                                              \
+  template <auto t, auto u>                                                                                            \
+  [[nodiscard]] ART_SPECIFIER consteval C<(t OP u)> operator OP(C<t>, C<u>) {                                          \
+    return {};                                                                                                         \
+  }
+
+ART_LEFT_UNARY_OP(+);
+ART_LEFT_UNARY_OP(-);
+ART_LEFT_UNARY_OP(~);
+ART_LEFT_UNARY_OP(!);
+ART_LEFT_UNARY_OP(*);
+
+ART_BINARY_OP(+);
+ART_BINARY_OP(-);
+ART_BINARY_OP(*);
+ART_BINARY_OP(/);
+ART_BINARY_OP(%);
+ART_BINARY_OP(&);
+ART_BINARY_OP(|);
+ART_BINARY_OP(^);
+ART_BINARY_OP(<<);
+ART_BINARY_OP(>>);
+
+ART_BINARY_OP(&&);
+ART_BINARY_OP(||);
+
+ART_BINARY_OP(==);
+ART_BINARY_OP(!=);
+ART_BINARY_OP(>);
+ART_BINARY_OP(<);
+ART_BINARY_OP(>=);
+ART_BINARY_OP(<=);
+
+#undef ART_LEFT_UNARY_OP
+#undef ART_RIGHT_UNARY_OP
+#undef ART_BINARY_OP
 
 } // namespace arietta
