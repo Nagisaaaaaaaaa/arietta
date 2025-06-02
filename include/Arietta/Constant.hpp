375bc26e0886d21cf8bcd85586615dee0e76cb9c
Yiheng Wu <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2025-06-02 21:06:21 +0800

    feat(Constant): Support operators for `C`. (#22)
    1. Implement operators for `(op C)`, `(C op)`, and `(C op C)`.
    2. Implement operator special cases between `C` and other types.

--- include/Arietta/Constant.hpp
+++ include/Arietta/Constant.hpp
@@ -20,7 +20,147 @@ struct C {
 
   static constexpr auto value = v;
 
-  [[nodiscard]] ART_SPECIFIER constexpr operator value_type() const noexcept { return value; }
+  [[nodiscard]] ART_SPECIFIER consteval operator value_type() const noexcept { return value; }
 };
+
+//
+//
+//
+// Operators for `(op C)`, `(C op)`, and `(C op C)`.
+
+#if defined(ART_CONSTANT_LEFT_UNARY_OP) || defined(ART_CONSTANT_RIGHT_UNARY_OP) || defined(ART_CONSTANT_BINARY_OP)
+static_assert(false, "Some macros conflict with Arietta");
+#endif
+
+#define ART_CONSTANT_LEFT_UNARY_OP(OP)                                                                                 \
+  template <auto t>                                                                                                    \
+  [[nodiscard]] ART_SPECIFIER consteval C<(OP t)> operator OP(C<t>) {                                                  \
+    return {};                                                                                                         \
+  }
+#define ART_CONSTANT_RIGHT_UNARY_OP(OP)                                                                                \
+  template <auto t>                                                                                                    \
+  [[nodiscard]] ART_SPECIFIER consteval C<(t OP)> operator OP(C<t>) {                                                  \
+    return {};                                                                                                         \
+  }
+#define ART_CONSTANT_BINARY_OP(OP)                                                                                     \
+  template <auto t, auto u>                                                                                            \
+  [[nodiscard]] ART_SPECIFIER consteval C<(t OP u)> operator OP(C<t>, C<u>) {                                          \
+    return {};                                                                                                         \
+  }
+
+ART_CONSTANT_LEFT_UNARY_OP(+);
+ART_CONSTANT_LEFT_UNARY_OP(-);
+ART_CONSTANT_LEFT_UNARY_OP(~);
+ART_CONSTANT_LEFT_UNARY_OP(!);
+ART_CONSTANT_LEFT_UNARY_OP(*);
+
+ART_CONSTANT_BINARY_OP(+);
+ART_CONSTANT_BINARY_OP(-);
+ART_CONSTANT_BINARY_OP(*);
+ART_CONSTANT_BINARY_OP(/);
+ART_CONSTANT_BINARY_OP(%);
+ART_CONSTANT_BINARY_OP(&);
+ART_CONSTANT_BINARY_OP(|);
+ART_CONSTANT_BINARY_OP(^);
+ART_CONSTANT_BINARY_OP(<<);
+ART_CONSTANT_BINARY_OP(>>);
+
+ART_CONSTANT_BINARY_OP(&&);
+ART_CONSTANT_BINARY_OP(||);
+
+ART_CONSTANT_BINARY_OP(==);
+ART_CONSTANT_BINARY_OP(!=);
+ART_CONSTANT_BINARY_OP(>);
+ART_CONSTANT_BINARY_OP(<);
+ART_CONSTANT_BINARY_OP(>=);
+ART_CONSTANT_BINARY_OP(<=);
+
+#undef ART_CONSTANT_LEFT_UNARY_OP
+#undef ART_CONSTANT_RIGHT_UNARY_OP
+#undef ART_CONSTANT_BINARY_OP
+
+//
+//
+//
+// Operator special cases between `C` and other types.
+
+#if defined(ART_CONSTANT_OP_RETURN)
+static_assert(false, "Some macros conflict with Arietta");
+#endif
+
+#define ART_CONSTANT_OP_RETURN(EXPR, EXPECT)                                                                           \
+  constexpr auto res = (EXPR);                                                                                         \
+  static_assert(                                                                                                       \
+      static_cast<decltype(EXPECT)>(res) == (EXPECT), "The given type cannot be used in the operators of `C`"          \
+  );                                                                                                                   \
+  return C<res> {}
+
+template <auto t, typename U>
+  requires(t == 0)
+[[nodiscard]] ART_SPECIFIER consteval auto operator*(C<t>, U) {
+  ART_CONSTANT_OP_RETURN(t * static_cast<U>(1), 0);
+}
+
+template <typename U, auto t>
+  requires(t == 0)
+[[nodiscard]] ART_SPECIFIER consteval auto operator*(U, C<t>) {
+  ART_CONSTANT_OP_RETURN(static_cast<U>(1) * t, 0);
+}
+
+template <auto t, typename U>
+  requires(t == 0)
+[[nodiscard]] ART_SPECIFIER consteval auto operator/(C<t>, U) {
+  ART_CONSTANT_OP_RETURN(t / static_cast<U>(1), 0);
+}
+
+template <typename U, auto t>
+  requires(t == 1 || t == -1)
+[[nodiscard]] ART_SPECIFIER consteval auto operator%(U, C<t>) {
+  ART_CONSTANT_OP_RETURN(static_cast<U>(1) % t, 0);
+}
+
+template <auto t, typename U>
+  requires(t == 0)
+[[nodiscard]] ART_SPECIFIER consteval auto operator%(C<t>, U) {
+  ART_CONSTANT_OP_RETURN(t % static_cast<U>(1), 0);
+}
+
+template <auto t, typename U>
+  requires(t == 0)
+[[nodiscard]] ART_SPECIFIER consteval auto operator&(C<t>, U) {
+  ART_CONSTANT_OP_RETURN(t & static_cast<U>(1), 0);
+}
+
+template <typename U, auto t>
+  requires(t == 0)
+[[nodiscard]] ART_SPECIFIER consteval auto operator&(U, C<t>) {
+  ART_CONSTANT_OP_RETURN(static_cast<U>(1) & t, 0);
+}
+
+template <auto t, typename U>
+  requires(!static_cast<bool>(t))
+[[nodiscard]] ART_SPECIFIER consteval auto operator&&(C<t>, U) {
+  ART_CONSTANT_OP_RETURN(t && static_cast<U>(true), false);
+}
+
+template <typename U, auto t>
+  requires(!static_cast<bool>(t))
+[[nodiscard]] ART_SPECIFIER consteval auto operator&&(U, C<t>) {
+  ART_CONSTANT_OP_RETURN(static_cast<U>(true) && t, false);
+}
+
+template <auto t, typename U>
+  requires(static_cast<bool>(t))
+[[nodiscard]] ART_SPECIFIER consteval auto operator||(C<t>, U) {
+  ART_CONSTANT_OP_RETURN(t || static_cast<U>(false), true);
+}
+
+template <typename U, auto t>
+  requires(static_cast<bool>(t))
+[[nodiscard]] ART_SPECIFIER consteval auto operator||(U, C<t>) {
+  ART_CONSTANT_OP_RETURN(static_cast<U>(false) || t, true);
+}
+
+#undef ART_CONSTANT_OP_RETURN
 
 } // namespace arietta
--- tests/Constant.cpp
+++ tests/Constant.cpp
@@ -8,7 +8,7 @@ using namespace boost::ut;
 namespace {
 
 suite<"Constant"> _ = [] {
-  "Instantiation"_test = [] {
+  "Instantiations"_test = [] {
     static_assert(std::is_same_v<C<0>::type, C<0>>);
     static_assert(std::is_same_v<C<0>::value_type, int>);
     static_assert(C<0>::value == 0);
@@ -44,6 +44,172 @@ suite<"Constant"> _ = [] {
       constexpr double v = c;
       static_assert(v == -3.0);
     }
+  };
+
+  //
+  //
+  //
+  "Implicit Casts By Operators"_test = [] {
+    static_assert(std::is_same_v<decltype(-3 + C<-3>{}), int>);
+    static_assert(std::is_same_v<decltype(0 + C<-3>{}), int>);
+    static_assert(std::is_same_v<decltype(3 + C<-3>{}), int>);
+    static_assert(std::is_same_v<decltype(-3 + C<0>{}), int>);
+    static_assert(std::is_same_v<decltype(0 + C<0>{}), int>);
+    static_assert(std::is_same_v<decltype(3 + C<0>{}), int>);
+    static_assert(std::is_same_v<decltype(-3 + C<3>{}), int>);
+    static_assert(std::is_same_v<decltype(0 + C<3>{}), int>);
+    static_assert(std::is_same_v<decltype(3 + C<3>{}), int>);
+
+    static_assert(std::is_same_v<decltype(C<-3>{} + -3), int>);
+    static_assert(std::is_same_v<decltype(C<0>{} + -3), int>);
+    static_assert(std::is_same_v<decltype(C<3>{} + -3), int>);
+    static_assert(std::is_same_v<decltype(C<-3>{} + 0), int>);
+    static_assert(std::is_same_v<decltype(C<0>{} + 0), int>);
+    static_assert(std::is_same_v<decltype(C<3>{} + 0), int>);
+    static_assert(std::is_same_v<decltype(C<-3>{} + 3), int>);
+    static_assert(std::is_same_v<decltype(C<0>{} + 3), int>);
+    static_assert(std::is_same_v<decltype(C<3>{} + 3), int>);
+
+    expect(-3 + C<-3>{} == -6);
+    expect(0 + C<-3>{} == -3);
+    expect(3 + C<-3>{} == 0);
+    expect(-3 + C<0>{} == -3);
+    expect(0 + C<0>{} == 0);
+    expect(3 + C<0>{} == 3);
+    expect(-3 + C<3>{} == 0);
+    expect(0 + C<3>{} == 3);
+    expect(3 + C<3>{} == 6);
+
+    expect(C<-3>{} + -3 == -6);
+    expect(C<0>{} + -3 == -3);
+    expect(C<3>{} + -3 == 0);
+    expect(C<-3>{} + 0 == -3);
+    expect(C<0>{} + 0 == 0);
+    expect(C<3>{} + 0 == 3);
+    expect(C<-3>{} + 3 == 0);
+    expect(C<0>{} + 3 == 3);
+    expect(C<3>{} + 3 == 6);
+  };
+
+  //
+  //
+  //
+  "Operator Common Cases"_test = [] {
+    static_assert(std::is_same_v<decltype(+C<-3>{}), C<-3>>);
+    static_assert(std::is_same_v<decltype(+C<0>{}), C<0>>);
+    static_assert(std::is_same_v<decltype(+C<3>{}), C<3>>);
+
+    static_assert(std::is_same_v<decltype(-C<-3>{}), C<3>>);
+    static_assert(std::is_same_v<decltype(-C<0>{}), C<0>>);
+    static_assert(std::is_same_v<decltype(-C<3>{}), C<-3>>);
+
+    static_assert(C<-3>{});
+    static_assert(!C<0>{});
+    static_assert(C<3>{});
+    static_assert(std::is_same_v<decltype(!C<-3>{}), C<false>>);
+    static_assert(std::is_same_v<decltype(!C<0>{}), C<true>>);
+    static_assert(std::is_same_v<decltype(!C<3>{}), C<false>>);
+
+    static_assert(std::is_same_v<decltype(C<-3>{} + C<-3>{}), C<-6>>);
+    static_assert(std::is_same_v<decltype(C<0>{} + C<-3>{}), C<-3>>);
+    static_assert(std::is_same_v<decltype(C<3>{} + C<-3>{}), C<0>>);
+    static_assert(std::is_same_v<decltype(C<-3>{} + C<0>{}), C<-3>>);
+    static_assert(std::is_same_v<decltype(C<0>{} + C<0>{}), C<0>>);
+    static_assert(std::is_same_v<decltype(C<3>{} + C<0>{}), C<3>>);
+    static_assert(std::is_same_v<decltype(C<-3>{} + C<3>{}), C<0>>);
+    static_assert(std::is_same_v<decltype(C<0>{} + C<3>{}), C<3>>);
+    static_assert(std::is_same_v<decltype(C<3>{} + C<3>{}), C<6>>);
+
+    static_assert(std::is_same_v<decltype(C<-3>{} - C<-3>{}), C<0>>);
+    static_assert(std::is_same_v<decltype(C<0>{} - C<-3>{}), C<3>>);
+    static_assert(std::is_same_v<decltype(C<3>{} - C<-3>{}), C<6>>);
+    static_assert(std::is_same_v<decltype(C<-3>{} - C<0>{}), C<-3>>);
+    static_assert(std::is_same_v<decltype(C<0>{} - C<0>{}), C<0>>);
+    static_assert(std::is_same_v<decltype(C<3>{} - C<0>{}), C<3>>);
+    static_assert(std::is_same_v<decltype(C<-3>{} - C<3>{}), C<-6>>);
+    static_assert(std::is_same_v<decltype(C<0>{} - C<3>{}), C<-3>>);
+    static_assert(std::is_same_v<decltype(C<3>{} - C<3>{}), C<0>>);
+
+    static_assert(std::is_same_v<decltype(C<-3>{} * C<-3>{}), C<9>>);
+    static_assert(std::is_same_v<decltype(C<0>{} * C<-3>{}), C<0>>);
+    static_assert(std::is_same_v<decltype(C<3>{} * C<-3>{}), C<-9>>);
+    static_assert(std::is_same_v<decltype(C<-3>{} * C<0>{}), C<0>>);
+    static_assert(std::is_same_v<decltype(C<0>{} * C<0>{}), C<0>>);
+    static_assert(std::is_same_v<decltype(C<3>{} * C<0>{}), C<0>>);
+    static_assert(std::is_same_v<decltype(C<-3>{} * C<3>{}), C<-9>>);
+    static_assert(std::is_same_v<decltype(C<0>{} * C<3>{}), C<0>>);
+    static_assert(std::is_same_v<decltype(C<3>{} * C<3>{}), C<9>>);
+
+    static_assert(std::is_same_v<decltype(C<-3>{} / C<-3>{}), C<1>>);
+    static_assert(std::is_same_v<decltype(C<0>{} / C<-3>{}), C<0>>);
+    static_assert(std::is_same_v<decltype(C<3>{} / C<-3>{}), C<-1>>);
+    static_assert(std::is_same_v<decltype(C<-3>{} / C<3>{}), C<-1>>);
+    static_assert(std::is_same_v<decltype(C<0>{} / C<3>{}), C<0>>);
+    static_assert(std::is_same_v<decltype(C<3>{} / C<3>{}), C<1>>);
+  };
+
+  //
+  //
+  //
+  "Operator Special Cases"_test = [] {
+    static_assert(std::is_same_v<decltype(C<0>{} * 3), C<0>>);
+    static_assert(std::is_same_v<decltype(C<0.0F>{} * 3), C<0.0F>>);
+    static_assert(std::is_same_v<decltype(C<0>{} * 3.0), C<0.0>>);
+    static_assert(std::is_same_v<decltype(C<0.0F>{} * 3.0), C<0.0>>);
+
+    static_assert(std::is_same_v<decltype(3 * C<0>{}), C<0>>);
+    static_assert(std::is_same_v<decltype(3.0F * C<0>{}), C<0.0F>>);
+    static_assert(std::is_same_v<decltype(3 * C<0.0>{}), C<0.0>>);
+    static_assert(std::is_same_v<decltype(3.0F * C<0.0>{}), C<0.0>>);
+
+    static_assert(std::is_same_v<decltype(C<0>{} / 3), C<0>>);
+    static_assert(std::is_same_v<decltype(C<0.0F>{} / 3), C<0.0F>>);
+    static_assert(std::is_same_v<decltype(C<0>{} / 3.0), C<0.0>>);
+    static_assert(std::is_same_v<decltype(C<0.0F>{} / 3.0), C<0.0>>);
+
+    static_assert(std::is_same_v<decltype(3 % C<1>{}), C<0>>);
+    static_assert(std::is_same_v<decltype(3U % C<1>{}), C<0U>>);
+    static_assert(std::is_same_v<decltype(3 % C<1U>{}), C<0U>>);
+    static_assert(std::is_same_v<decltype(3U % C<1U>{}), C<0U>>);
+    static_assert(std::is_same_v<decltype(3 % C<-1>{}), C<0>>);
+    static_assert(std::is_same_v<decltype(3LL % C<-1>{}), C<0LL>>);
+    static_assert(std::is_same_v<decltype(3 % C<-1LL>{}), C<0LL>>);
+    static_assert(std::is_same_v<decltype(3LL % C<-1LL>{}), C<0LL>>);
+
+    static_assert(std::is_same_v<decltype(C<0>{} % 3), C<0>>);
+    static_assert(std::is_same_v<decltype(C<0U>{} % 3), C<0U>>);
+    static_assert(std::is_same_v<decltype(C<0>{} % 3U), C<0U>>);
+    static_assert(std::is_same_v<decltype(C<0U>{} % 3U), C<0U>>);
+
+    static_assert(std::is_same_v<decltype(C<0>{} & 3), C<0>>);
+    static_assert(std::is_same_v<decltype(C<0U>{} & 3), C<0U>>);
+    static_assert(std::is_same_v<decltype(C<0>{} & 3U), C<0U>>);
+    static_assert(std::is_same_v<decltype(C<0U>{} & 3U), C<0U>>);
+
+    static_assert(std::is_same_v<decltype(3 & C<0>{}), C<0>>);
+    static_assert(std::is_same_v<decltype(3U & C<0>{}), C<0U>>);
+    static_assert(std::is_same_v<decltype(3 & C<0U>{}), C<0U>>);
+    static_assert(std::is_same_v<decltype(3U & C<0U>{}), C<0U>>);
+
+    static_assert(std::is_same_v<decltype(C<0>{} && 3), C<false>>);
+    static_assert(std::is_same_v<decltype(C<0.0F>{} && 3), C<false>>);
+    static_assert(std::is_same_v<decltype(C<0>{} && 3.0), C<false>>);
+    static_assert(std::is_same_v<decltype(C<0.0F>{} && 3.0), C<false>>);
+
+    static_assert(std::is_same_v<decltype(3 && C<0>{}), C<false>>);
+    static_assert(std::is_same_v<decltype(3.0F && C<0>{}), C<false>>);
+    static_assert(std::is_same_v<decltype(3 && C<0.0>{}), C<false>>);
+    static_assert(std::is_same_v<decltype(3.0F && C<0.0>{}), C<false>>);
+
+    static_assert(std::is_same_v<decltype(C<1>{} || 3), C<true>>);
+    static_assert(std::is_same_v<decltype(C<1.0F>{} || 3), C<true>>);
+    static_assert(std::is_same_v<decltype(C<1>{} || 3.0), C<true>>);
+    static_assert(std::is_same_v<decltype(C<1.0F>{} || 3.0), C<true>>);
+
+    static_assert(std::is_same_v<decltype(3 || C<1>{}), C<true>>);
+    static_assert(std::is_same_v<decltype(3.0F || C<1>{}), C<true>>);
+    static_assert(std::is_same_v<decltype(3 || C<1.0>{}), C<true>>);
+    static_assert(std::is_same_v<decltype(3.0F || C<1.0>{}), C<true>>);
   };
 };
 
