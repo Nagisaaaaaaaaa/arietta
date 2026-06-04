6a94a5834f2eca90ed77e40c3b07a3a9aa11cb1a
Yiheng Wu <116617349+Nagisaaaaaaaaa@users.noreply.github.com>
2026-06-04 22:00:36 +0800

    Add concept `is::Integral`.

--- include/Arietta/Arietta.hpp
+++ include/Arietta/Arietta.hpp
@@ -59,6 +59,9 @@ concept Arithmetic = detail::arietta::IsArithmetic<T>::value;
 template <typename T>
 concept Empty = std::is_empty_v<T>;
 
+template <typename T>
+concept Integral = std::integral<T>;
+
 template <typename F, typename... Args>
 concept Invocable = std::invocable<F, Args...>;
 
@@ -77,6 +80,9 @@ concept Arithmetic = !is::Arithmetic<T>;
 
 template <typename T>
 concept Empty = !is::Empty<T>;
+
+template <typename T>
+concept Integral = !is::Integral<T>;
 
 template <typename F, typename... Args>
 concept Invocable = !is::Invocable<F, Args...>;
